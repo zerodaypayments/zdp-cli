cfd200806f769525b0217f09200f97d1d1749da6
Sergey Nechaev <sergey.nechaev@equifax.com>
2018-02-27 17:46:36 +1100

    using args parser

--- src/zdp-cli.cpp
+++ src/zdp-cli.cpp
@@ -1,57 +1,46 @@
-/**
- * 	zdp fee - returns <fee>
- *	zdp newaccount - returns <secretkey>
- *	zdp ping - returns <current time>
- *	zdp keys <secretkey> - return key pair for the seed
- *	zdp balance <secretkey> - returns <seed> balance
- *	zdp newaddress <secretkey> - returns unique address for the transfer
- *	zdp tx <uuid> - returns tx information
- *	TODO zdp restore <comma-separated-list-of-words> - returns seed
- *	TODO zdp transfer <from> <to> <amount> <memo> <secretkey> - return TX UUID or error
- *	TODO zdp txfrom <address>
- *	TODO zdp txto <address>
- *	TODO zdp txfrommemo <from> <memo>
- *	TODO zdp txtomemo <to> <memo>
- *	TODO zdp txlist <seed> [number-of-transactions] - returns last of max of 100 latest tx
- */
-
-#include <cstdlib>
 #include <iostream>
-#include <string>
-#include <vector>
-
+#include "args.hpp"
 #include "utils/http_utils.h"
 #include "json.hpp"
 
+args::Group arguments("arguments");
+args::HelpFlag h(arguments, "help", "help", { 'h', "help" });
+
 using nlohmann::json;
 
-void help() {
-}
-
-int main(int argc, char** argv) {
-
-	std::vector<std::string> args;
-
-	for (int i = 0; i < argc; ++i) {
-		args.push_back(std::string { argv[i] });
-	}
-
-	if (args.size() == 1) {
-		help();
-		return EXIT_SUCCESS;
-	}
-
-	std::string host = "http://localhost";
+std::string host = "http://localhost";
 //	std::string host = "http://api.zdp.io";
 
-	const auto arg_1 = args[1];
+const auto timeout = 10;
+const auto user_agent = "zdp-cli";
 
-	const auto timeout = 10;
-	const auto user_agent = "zdp-cli";
+int main(int argc, const char **argv) {
 
-	zdp::utils::httpclient http_client;
+	args::ArgumentParser p("ZDP command line interface");
+	args::Group commands(p, "commands");
 
-	if (arg_1 == "ping") {
+	args::Command fee(commands, "fee", "Get the current network fee", [&](args::Subparser &parser)
+	{
+
+		parser.Parse();
+
+		zdp::utils::httpclient http_client;
+
+		auto resp = http_client.get(host + "/api/v1/fee", timeout, user_agent);
+
+		if (!resp.error) {
+			auto json = json::parse(resp.data);
+			std::cout << json.dump(4) << std::endl;
+		}
+
+	});
+
+	args::Command ping(commands, "ping", "Ping the network", [&](args::Subparser &parser)
+	{
+
+		parser.Parse();
+
+		zdp::utils::httpclient http_client;
 
 		auto resp = http_client.get(host + "/ping", timeout, user_agent);
 		if (!resp.error) {
@@ -59,78 +48,101 @@ int main(int argc, char** argv) {
 			std::cout << json.dump(4) << std::endl;
 		}
 
-	} else if (arg_1 == "fee") {
+	});
 
-		auto resp = http_client.get(host + "/api/v1/fee", timeout, user_agent);
+	args::Command newAccount(commands, "newaccount", "Create new account", [&](args::Subparser &parser)
+	{
+
+		args::ValueFlag<std::string> lang(parser, "LANGUAGE", "Mnemonics language", {"lang"});
+
+		parser.Parse();
+
+		auto url = host + "/api/v1/account/seed";
+
+		auto language = args::get(lang);
+
+		url += "?lang=" + language;
+
+		zdp::utils::httpclient http_client;
+
+		auto resp = http_client.get(url, timeout, user_agent);
+
 		if (!resp.error) {
 			auto json = json::parse(resp.data);
 			std::cout << json.dump(4) << std::endl;
 		}
 
-	} else if (arg_1 == "newaccount") {
+	});
 
-		auto resp = http_client.get(host + "/api/v1/account/seed", timeout, user_agent);
+	args::Command keys(commands, "keys", "Generate public and private key pair for a secret key", [&](args::Subparser &parser)
+	{
 
+		args::ValueFlag<std::string> keyValue(parser, "SECRETKEY", "Secret key", {"key"}, args::Options::Required);
+
+		parser.Parse();
+
+		auto key = args::get(keyValue);
+
+		zdp::utils::httpclient http_client;
+
+		auto resp = http_client.get(host + "/api/v1/account/keys/" + key, timeout, user_agent);
 		if (!resp.error) {
 			auto json = json::parse(resp.data);
 			std::cout << json.dump(4) << std::endl;
 		}
 
-	} else if (arg_1 == "keys") {
+	});
 
-		if (args.size() < 3) {
-			std::cerr << "Please, specify the <seed> value\n";
-			return EXIT_FAILURE;
-		}
+	args::Command balance(commands, "balance", "Get account's balance by a secret key", [&](args::Subparser &parser)
+	{
 
-		const auto arg_2 = args[2];
+		args::ValueFlag<std::string> keyValue(parser, "SECRETKEY", "Secret key", {"key"}, args::Options::Required);
 
-		auto resp = http_client.get(host + "/api/v1/account/keys/" + arg_2, timeout, user_agent);
+		parser.Parse();
+
+		auto key = args::get(keyValue);
+
+		zdp::utils::httpclient http_client;
+
+		auto resp = http_client.get(host + "/api/v1/seed/balance/" + key, timeout, user_agent);
 		if (!resp.error) {
 			auto json = json::parse(resp.data);
 			std::cout << json.dump(4) << std::endl;
 		}
 
-	} else if (arg_1 == "balance") {
+	});
 
-		if (args.size() < 3) {
-			std::cerr << "Please, specify the <seed> value\n";
-			return EXIT_FAILURE;
-		}
+	args::Command address(commands, "newaddress", "Generate a unique address for an existing account", [&](args::Subparser &parser)
+	{
 
-		const auto arg_2 = args[2];
+		args::ValueFlag<std::string> keyValue(parser, "SECRETKEY", "Secret key", {"key"}, args::Options::Required);
 
-		auto resp = http_client.get(host + "/api/v1/seed/balance/" + arg_2, timeout, user_agent);
+		parser.Parse();
+
+		auto key = args::get(keyValue);
+
+		zdp::utils::httpclient http_client;
+
+		auto resp = http_client.get(host + "/api/v1/seed/address/" + key, timeout, user_agent);
 		if (!resp.error) {
 			auto json = json::parse(resp.data);
 			std::cout << json.dump(4) << std::endl;
 		}
 
-	} else if (arg_1 == "newaddress") {
+	});
 
-		const auto arg_2 = args[2];
+	args::Command tx(commands, "tx", "Get transaction information by UUID", [&](args::Subparser &parser)
+	{
 
-		if (args.size() < 3) {
-			std::cerr << "Please, specify the <seed> value\n";
-			return EXIT_FAILURE;
-		}
+		args::ValueFlag<std::string> uuidValue(parser, "UUID", "Transaction UUID", {"uuid"}, args::Options::Required);
 
-		auto resp = http_client.get(host + "/api/v1/seed/address/" + arg_2, timeout, user_agent);
-		if (!resp.error) {
-			auto json = json::parse(resp.data);
-			std::cout << json.dump(4) << std::endl;
-		}
+		parser.Parse();
 
-	} else if (arg_1 == "tx") {
+		auto uuid = args::get(uuidValue);
 
-		if (args.size() < 3) {
-			std::cerr << "Please, specify the <transaction UUID>\n";
-			return EXIT_FAILURE;
-		}
+		zdp::utils::httpclient http_client;
 
-		const auto arg_2 = args[2];
-
-		auto resp = http_client.get(host + "/api/v1/tx/" + arg_2, timeout, user_agent);
+		auto resp = http_client.get(host + "/api/v1/tx/" + uuid, timeout, user_agent);
 
 		if (!resp.error) {
 
@@ -138,9 +150,17 @@ int main(int argc, char** argv) {
 			std::cout << json.dump(4) << std::endl;
 		}
 
-	};
+	});
 
+	args::GlobalOptions globals(p, arguments);
+
+	try {
+		p.ParseCLI(argc, argv);
+	} catch (args::Help) {
+		std::cout << p;
+	} catch (args::Error& e) {
+		std::cerr << e.what() << std::endl << p;
+		return EXIT_FAILURE;
+	}
 	return EXIT_SUCCESS;
-
 }
-
